4d6c725168958c8c4cd9f40926ff53cf3999e119
devcusn <hitunc@outlook.com>
2023-04-16 10:09:33 +0300

    feat: husky add

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,25 @@ module.exports = {
     es2021: true,
     jest: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: './tsconfig.json'
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'prettier'
-  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     '@typescript-eslint/dot-notation': ['off'],
-    '@typescript-eslint/parser': ['off']
+    '@typescript-eslint/parser': ['off'],
+    '@typescript-eslint/no-throw-literal': ['off'],
+    '@typescript-eslint/await-thenable': ['off'],
+    '@typescript-eslint/consistent-type-exports': ['off'],
+    '@typescript-eslint/no-base-to-string': ['off'],
+    '@typescript-eslint/triple-slash-reference': ['off'],
+    '@typescript-eslint/consistent-type-definitions': ['off'],
+    '@typescript-eslint/array-type': ['off'],
+    '@typescript-eslint/consistent-type-imports': ['off'],
+    '@typescript-eslint/strict-boolean-expressions': ['off']
   }
 }
